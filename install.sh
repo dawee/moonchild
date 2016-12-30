41f14095eeccf860549aaa2c80c1bb1be83545f9
David Corticchiato <david.corticchiato@gmail.com>
2016-12-30 11:49:40 +0100

    keep only lua

--- install.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install lua5.1 gcc-avr binutils-avr avr-libc avrdude
-
-
--- test/fixtures/out-to-program.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def out_to_program(out_filename, program_filename):
-    pass
-
-if __name__ == '__main__':
-    out_to_program(*sys.argv[1:])
