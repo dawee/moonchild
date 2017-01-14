797a1e99efa92f0dfc73d3670e933ff2fec31a19
David Corticchiato <david.corticchiato@gmail.com>
2017-01-14 12:49:54 +0100

    debug simulator

--- src/moonchild/simulator_runner.cpp
+++ src/moonchild/simulator_runner.cpp
@@ -10,25 +10,38 @@ char progmem_read(PGMEM_ADDRESS mem_addr, uint16_t offset) {
 }
 
 
+void arduboy_print(moon_closure * closure, BOOL has_params) {
+  moon_reference buf_ref;
+  char buffer[255];
+  
+  moon_create_value_copy(&buf_ref, closure->registers[closure->base]);
+  moon_ref_to_cstr(buffer, &buf_ref);
+
+  printf("%s", buffer);
+
+  if (buf_ref.is_copy == TRUE) moon_delete_value((moon_value *) buf_ref.value_addr);  
+}
+
 void moon_arch_run(PGMEM_ADDRESS prototype_addr) {
   BOOL has_update;
+
+  moon_add_global_api_func("arduboy_print", &arduboy_print);
 
   moon_reference key_reference;
   moon_reference update_reference;
   moon_closure * closure = moon_create_closure(prototype_addr);
 
   moon_create_string_value(&key_reference, "update");
-
   moon_run_closure(closure);
-
   has_update = moon_find_closure_value(&update_reference, closure, &key_reference);
 
-  if (has_update == TRUE && MOON_AS_VALUE(&update_reference)->type == LUA_CLOSURE) {
-
-    moon_run_closure(MOON_AS_CLOSURE(&update_reference));
-    moon_debug("update exists\n");
-  }
+  moon_run_closure(MOON_AS_CLOSURE(&update_reference));
+  moon_run_closure(MOON_AS_CLOSURE(&update_reference));
+  moon_run_closure(MOON_AS_CLOSURE(&update_reference));
+  moon_run_closure(MOON_AS_CLOSURE(&update_reference));
+  moon_run_closure(MOON_AS_CLOSURE(&update_reference));
 }
+
 
 int main() {
   moon_init();
