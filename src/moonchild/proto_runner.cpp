07e0b3d63b04a7c89ee36d29e0fe816c16ecd607
David Corticchiato <david.corticchiato@gmail.com>
2017-01-16 13:14:46 +0100

    merge runners

--- src/moonchild/proto_runner.cpp
+++ src/moonchild/proto_runner.cpp
@@ -1,11 +1,19 @@
 #ifdef MOONCHILD_SIMULATOR
+#include <stdio.h>
+#else
+#include "Arduboy.h"
+static Arduboy arduboy;
+#endif
+
 
 #include "moonchild.h"
-#include <stdio.h>
+
 
 static BOOL has_update;
 static moon_reference update_reference;
 static moon_closure * closure;
+
+#ifdef MOONCHILD_SIMULATOR
 
 char progmem_read(PGMEM_ADDRESS mem_addr, uint16_t offset) {
   char * bytes = (char *) mem_addr;
@@ -32,6 +40,32 @@ void arduboy_print(moon_closure * closure, BOOL has_params) {
   if (buf3_ref.is_copy == TRUE) moon_delete_value((moon_value *) buf3_ref.value_addr);  
 }
 
+#else
+
+
+void arduboy_print(moon_closure * closure, BOOL has_params) {
+  moon_reference buf1_ref;
+  moon_reference buf2_ref;
+  moon_reference buf3_ref;
+  char buffer[16];
+  
+  moon_create_value_copy(&buf1_ref, closure->registers[closure->base]);
+  moon_create_value_copy(&buf2_ref, closure->registers[closure->base + 1]);
+  moon_create_value_copy(&buf3_ref, closure->registers[closure->base + 2]);
+  moon_ref_to_cstr(buffer, &buf3_ref);
+
+  arduboy.setCursor(MOON_AS_INT(&buf1_ref)->val, MOON_AS_INT(&buf2_ref)->val);
+  arduboy.print(F("o"));
+
+  if (buf1_ref.is_copy == TRUE) moon_delete_value((moon_value *) buf1_ref.value_addr);  
+  if (buf2_ref.is_copy == TRUE) moon_delete_value((moon_value *) buf2_ref.value_addr);  
+  if (buf3_ref.is_copy == TRUE) moon_delete_value((moon_value *) buf3_ref.value_addr);  
+}
+
+#endif
+
+
+
 void moon_arch_run(PGMEM_ADDRESS prototype_addr) {
   moon_add_global_api_func("arduboy_print", &arduboy_print);
 
@@ -43,6 +77,8 @@ void moon_arch_run(PGMEM_ADDRESS prototype_addr) {
   has_update = moon_find_closure_value(&update_reference, closure, &key_reference);
 }
 
+
+#ifdef MOONCHILD_SIMULATOR
 void moon_arch_update() {
   if (has_update == TRUE && MOON_AS_VALUE(&update_reference)->type == LUA_CLOSURE) {
     moon_run_closure(MOON_AS_CLOSURE(&update_reference), closure);
@@ -58,6 +94,20 @@ int main() {
   moon_arch_update();
   moon_arch_update();
   return 0;
+}
+
+#else
+
+void moon_arch_update() {
+  if (!(arduboy.nextFrame())) return;
+  
+  arduboy.clear();
+
+  if (has_update == TRUE && MOON_AS_VALUE(&update_reference)->type == LUA_CLOSURE) {
+    moon_run_closure(MOON_AS_CLOSURE(&update_reference), closure);
+  }
+
+  arduboy.display();
 }
 
 #endif
