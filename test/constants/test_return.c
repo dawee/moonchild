1b2915fe97bcc37a6b9eba18133b9baaae6bda3c
David Corticchiato <david.corticchiato@gmail.com>
2017-01-27 17:43:09 +0100

    test const return

--- test/constants/test_return.c
+++ test/constants/test_return.c
@@ -4,15 +4,39 @@
 
 moon_closure * create_main_closure();
 
-TEST should_not_crash(void) {
+TEST should_return_42(void) {
+    moon_reference buf_ref;
+    moon_closure * closure = create_main_closure();
+
+    moon_run_closure(closure, NULL);
+    moon_create_value_copy(&buf_ref, &(closure->result));
+
+    ASSERT_EQ(TRUE, MOON_IS_INT(&buf_ref));
+    ASSERT_EQ(42, MOON_AS_INT(&buf_ref)->val);
+
+    moon_delete_value((moon_value *) closure);
+    GREATEST_PASS();
+
+    if (buf_ref.is_copy == TRUE) moon_delete_value((moon_value *) buf_ref.value_addr);
+}
+
+TEST should_not_leak_at_reload(void) {
+    int memo;
+
     moon_closure * closure = create_main_closure();
     moon_run_closure(closure, NULL);
+
+    memo = moon_monitor_get_total();
+    moon_run_closure(closure, NULL);
+    ASSERT_EQ(memo, moon_monitor_get_total());
+
     moon_delete_value((moon_value *) closure);
     GREATEST_PASS();
 }
 
 SUITE(ReturnConst) {
-    RUN_TEST(should_not_crash);
+    RUN_TEST(should_return_42);
+    RUN_TEST(should_not_leak_at_reload);
 }
 
 GREATEST_MAIN_DEFS();
