73287bbfd5a733b4c12fcfd44ef6c8ef5b67cdc8
David Corticchiato <david.corticchiato@gmail.com>
2017-01-29 14:38:44 +0100

    weak io simulation

---
+++ src/simulator/avr/io.c
@@ -0,0 +1,6 @@
+#include <avr/io.h>
+
+
+int _BV(int ddrb) {
+  return 0;
+}
---
+++ src/simulator/avr/io.h
@@ -0,0 +1,11 @@
+#ifndef SIM_AVR_IO_H
+#define SIM_AVR_IO_H
+
+int DDRB;
+int DDB5;
+int PORTB;
+int PORTB5;
+
+int _BV(int ddrb);
+
+#endif
